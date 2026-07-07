bed40d1b03b77c9e7d18dc1ac673ecf1d7985796
Heba Maher <hebamaher25@gmail.com>
2026-07-08 01:31:59 +0300

    add service model

---
+++ src/modules/service/service.model.js
@@ -0,0 +1,50 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../../core/db');
+const { Project } = require('../projects/projects.model');
+
+const Service = sequelize.define(
+  'Service',
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    project_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: Project,
+        key: 'id',
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    repository_url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    branch: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'main',
+    },
+  },
+  {
+    tableName: 'services',
+    timestamps: true,
+    indexes: [
+      {
+        unique: true,
+        fields: ['project_id', 'name'],
+      },
+    ],
+  }
+);
+
+Service.belongsTo(Project, { foreignKey: 'project_id', as: 'project' });
+Project.hasMany(Service, { foreignKey: 'project_id', as: 'services' });
+
+module.exports = { Service };
