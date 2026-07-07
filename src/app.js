2851cdc3cfe1802856712c54b1a4f371fd12232f
Heba Maher <hebamaher25@gmail.com>
2026-07-08 02:15:52 +0300

    add servicesRoutes to app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const authRoutes = require('./modules/auth/auth.routes');
 const projectsRoutes = require('./modules/projects/projects.routes');
+const servicesRoutes = require('./modules/service/service.routes');
 const errorHandler = require('./core/middlewares/errorHandler');
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.use('/auth', authRoutes);
 app.use('/projects', projectsRoutes);
+app.use('/services', servicesRoutes);
 
 app.use((req, res) => {
   res.status(404).json({ success: false, message: 'Route not found' });
