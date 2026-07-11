cad4eae4dced2580f825802d6dcc34fd5cc9d1b3
hafez599 <hafezadel599@gmail.com>
2026-07-11 16:57:07 +0300

    fix small issue

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -3,6 +3,7 @@ const app = require('./app');
 const sequelize = require('./config/db');
 require('./modules/auth/auth.model');
 require('./modules/projects/projects.model');
+require('./modules/github/github.model');
 
 const PORT = process.env.PORT || 5000;
 
