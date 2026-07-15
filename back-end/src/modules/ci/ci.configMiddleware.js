36d0bc5322a7306d713baef6490cccde81d4774c
jannawaelali-creator <janna.wael.ali@gmail.com>
2026-07-16 02:52:17 +0300

    delete aws_region and docker username password from data user gives as they are secrets

--- back-end/src/modules/ci/ci.configMiddleware.js
+++ back-end/src/modules/ci/ci.configMiddleware.js
@@ -2,24 +2,25 @@ const { validateCIConfig } = require('./ci.validation');
 const AppError = require('../../core/utils/AppError');
 
 
- 
+
 function validateCIConfigMiddleware(req, res, next) {
   try {
+    const body = req.body || {};
     const {
       pipelineName,
       triggerBranch,
       registry,
       imageName,
       awsEcrRegion,
-    } = req.body;
+    } = body;
 
-    const enableTrivy = req.body.enableTrivy !== undefined ? req.body.enableTrivy : req.body.enabletrivy;
-    const enableLint = req.body.enableLint !== undefined ? req.body.enableLint : req.body.enablelint;
-    const enableTests = req.body.enableTests !== undefined ? req.body.enableTests : req.body.enabletests;
-    const enableBuild = req.body.enableBuild !== undefined ? req.body.enableBuild : req.body.enablebuild;
-    const enableInstall = req.body.enableInstall !== undefined ? req.body.enableInstall : req.body.enableinstall;
+    const enableTrivy = body.enableTrivy !== undefined ? body.enableTrivy : body.enabletrivy;
+    const enableLint = body.enableLint !== undefined ? body.enableLint : body.enablelint;
+    const enableTests = body.enableTests !== undefined ? body.enableTests : body.enabletests;
+    const enableBuild = body.enableBuild !== undefined ? body.enableBuild : body.enablebuild;
+    const enableInstall = body.enableInstall !== undefined ? body.enableInstall : body.enableinstall;
 
-    const serviceId = req.params.serviceId || req.body.serviceId;
+    const serviceId = req.params.serviceId || body.serviceId;
 
     if (!serviceId) {
       throw new AppError('Service ID is required', 400);
