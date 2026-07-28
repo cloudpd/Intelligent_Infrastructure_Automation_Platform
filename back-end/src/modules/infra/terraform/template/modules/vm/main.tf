1ba26a8bfa114609b861fa7741f482f0742e1cb4
hafez599 <hafezadel599@gmail.com>
2026-07-29 01:30:16 +0300

    add SSM part with right roles for the vm

--- back-end/src/modules/infra/terraform/template/modules/vm/main.tf
+++ back-end/src/modules/infra/terraform/template/modules/vm/main.tf
@@ -9,6 +9,28 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+resource "aws_iam_role" "ssm_role" {
+  name = "${var.name}-ssm-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "${var.name}-ssm-profile"
+  role = aws_iam_role.ssm_role.name
+}
+
 # --- Security group: only the app's host_port is open. No SSH. ---
 resource "aws_security_group" "vm_sg" {
   name   = "${var.name}-sg"
@@ -36,6 +58,7 @@ resource "aws_instance" "vm" {
   subnet_id                   = var.subnet_id
   vpc_security_group_ids      = [aws_security_group.vm_sg.id]
   associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.ssm_profile.name
 
   user_data = templatefile("${path.module}/user_data.sh.tpl", {
     kind_cluster_name = var.kind_cluster_name
